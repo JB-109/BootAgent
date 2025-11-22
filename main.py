da827e36f72a5d55830a7e3c8f3f6c53fff8f856
JB <68579548+JB-109@users.noreply.github.com>
2025-11-22 21:50:35 +0530

    --verbose

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from google.genai import types
 
 load_dotenv()
 api_key = os.getenv("GEMINI_API_KEY")
-client = genai.Client(api_key="AIzaSyBek-wGgTymsHwVez338nw28Hd6zJHjIKU")
+client = genai.Client(api_key=api_key)
 
 #++++++++++
 
@@ -28,9 +28,9 @@ response = client.models.generate_content(
     model='gemini-2.0-flash', contents=messages
 )
 
-print(response.text)
-usage = response.usage_metadata
-
-print(f"Prompt tokens: {usage.prompt_token_count}")
-print(f"Response tokens: {usage.candidates_token_count}")
-
+if len(sys.argv) > 2 and sys.argv[2] == "--verbose":
+    print(f"User prompt: {prompt}")
+    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+else:
+    print(response.text)
